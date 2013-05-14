12b67b458dac1e69f9ae7ab2948373c22ef2fb50
Federico Panico <bpm230@gmail.com>
2013-05-13 22:16:32 -0300

    New docs

---
+++ doc/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "GrooveToggle.Button",
+        "GrooveToggle.myBgApp",
+        "GrooveToggle.myFgApp"
+    ],
+    "modules": [
+        "GrooveToggle"
+    ],
+    "allModules": [
+        {
+            "displayName": "GrooveToggle",
+            "name": "GrooveToggle",
+            "description": "*Groovetoggle* lets you control playback of Grooveshark from Opera."
+        }
+    ]
+} };
+});
